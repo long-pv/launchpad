db9b5e10230f86eb682ddca8cbfae7a30c2281a9
longpv <long05062000@gmail.com>
2025-04-03 12:44:15 +0700

    cải tiến container

---
+++ wp-content/themes/launch-pad/page-demo.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Template Name: Demo
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package launch-pad
+ */
+
+// header template
+get_header();
+?>
+
+<div class="container">
+    <div class="bg-light">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci suscipit nemo sequi porro, similique doloribus sapiente eaque accusamus, maxime ipsum cupiditate, fugit ut incidunt aliquid odit nam saepe? Repellendus, cum.
+    </div>
+</div>
+
+<?php
+get_footer();
