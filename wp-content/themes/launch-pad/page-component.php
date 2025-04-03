8e381a0076c37ac493eb4fabd9750a28d4089a3d
Youngboy55 <phamvanvu411.nd@gmail.com>
2025-04-03 10:27:13 +0700

    Task: page component

---
+++ wp-content/themes/launch-pad/page-component.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Template Name: Component
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package launch-pad
+ */
+
+// header template
+get_header();
+// flexible content
+get_template_part('template-parts/content-flexible');
+// footer template
+get_footer();
