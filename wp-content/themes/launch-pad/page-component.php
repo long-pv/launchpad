831d6392708aa6dd82698ed64a773d8e8cfc65fb
Youngboy55 <phamvanvu411.nd@gmail.com>
2025-04-03 16:35:35 +0700

    TASK: Button & Handbook

--- wp-content/themes/launch-pad/page-component.php
+++ wp-content/themes/launch-pad/page-component.php
@@ -18,12 +18,59 @@
 get_header();
 ?>
 
+<!-- Hand Book -->
 <section class="secSpace">
-    <div class="handbook">
+    <div class="container">
+        <div class="row">
+            <div class="text-center m-4" style="font-weight: 700;">
+                HAND BOOK
+            </div>
+            <?php for ($i = 0; $i < 4; $i++): ?>
+                <div class="col-lg-3">
+                    <div class="handbook">
+                        <a href="#" class="handbook__image">
+                            <img src="https://ocafe.net/wp-content/uploads/2024/10/anh-nen-may-tinh-4k-1.jpg"
+                                alt="International Student Handbook">
+                        </a>
+                        <a href="#" class="handbook__link">
+                            <h3 class="handbook__title">International Student Handbook</h3>
+                        </a>
+                    </div>
+                </div>
+            <?php endfor; ?>
+        </div>
+    </div>
+</section>
+
+<!-- Button Style -->
+<section class="secSpace">
+    <div class="container">
+
+        <div class="text-center m-4" style="font-weight: 700;">
+            BUTTON
+        </div>
+        <!-- style 1 -->
+        <button type="button" class="read_more">Read more</button>
+
+        <!-- style 2 -->
+        <button type="button" class="button_1">Read more</button>
+
+        <!-- style 3 -->
+        <button type="button" class="button_1 button_1__active">Read more</button>
 
     </div>
-    DDay la du an code chay
 </section>
+
+<!-- Banner -->
+<div class="banner">
+    <div class="banner__content">
+        <div class="banner__social">
+            <a href="#" class="banner__social-icon banner__social-icon--facebook"></a>
+            <a href="#" class="banner__social-icon banner__social-icon--youtube"></a>
+            <a href="#" class="banner__social-icon banner__social-icon--telegram"></a>
+        </div>
+    </div>
+</div>
 
 
 <?php
