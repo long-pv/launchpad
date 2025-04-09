bb5e1b85784bd2645bc10106cc43a4d8c2b86a29
Youngboy55 <phamvanvu411.nd@gmail.com>
2025-04-10 03:23:48 +0700

    Task: Search post

--- wp-content/themes/launch-pad/search.php
+++ wp-content/themes/launch-pad/search.php
@@ -15,21 +15,33 @@ get_header();
 	<div class="page_wrap">
 		<div class="page_inner">
 			<div class="page_body">
-				<?php
-				if (have_posts()) :
-					while (have_posts()) :
-						the_post();
-				?>
-						xxxx
-				<?php
+				<?php if (get_search_query()): ?>
+					<p class="list_post__title">
+						<?php _e("Search result for"); ?> <span
+							class="list_post__keyword"><?php echo esc_html(get_search_query()); ?></span>
+					</p>
+				<?php endif; ?>
 
-					endwhile;
-				endif;
-				?>
+				<div class="list_post__list">
+					<?php
+					if (have_posts()):
+						while (have_posts()):
+							the_post();
+							?>
+							<div class="list_post__item">
+								<a href="<?php the_permalink(); ?>" class="list_post__link">
+									<h3 class=" list_post__heading"><?php the_title(); ?></h3>
+								</a>
+								<p class="list_post__description"><?php echo get_the_excerpt(); ?></p>
+							</div>
+							<?php
+						endwhile;
+					endif;
+					?>
+				</div>
 			</div>
 		</div>
 	</div>
 </div>
-
 <?php
 get_footer();
